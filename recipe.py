a9a35013dc95d404ce642cc8587b3db5c107cd7e
gyamposudodzi <gyamposu@gmail.com>
2023-06-09 21:05:01 +0000

    made inputs of foods and it's detail

--- recipe.py
+++ recipe.py
@@ -2,6 +2,7 @@
 recipes = [
         {
             'name': 'Jollof Rice',
+            
             'ingredients': ['2 cups long-grain rice', 
                             '1 onion,ﬁnely chopped', 
                             '2 tomatoes, blended or ﬁnely chopped', 
@@ -17,6 +18,7 @@ recipes = [
                             '2 cups chicken or vegetable broth', 
                             'Salt to taste', 
                             'Optional: 1 cup of mixed vegetables(carrots, peas, bell peppers)'],
+            
             'instructions': [
                 "Rinse the rice under cold water until the water runs clear. Drain and set aside.",
                 "Heat the vegetable oil in a large pot or Dutch oven over medium heat.",
@@ -28,6 +30,7 @@ recipes = [
                 "Once the rice is cooked, fluff it gently with a fork.",
                 "Remove from heat and let it rest for a few minutes before serving."
                             ],
+            
             'nutrients': ['Calories: 300', 
                           'Protein: 10g', 
                           'Carbs: 50g', 
@@ -36,13 +39,15 @@ recipes = [
 
         {
             'name': 'Noodles',
+            
             'ingredients': ['chicken breast', 
                             'bell peppers', 
                             'zucchini', 
                             'olive oil', 
                             'salt', 
                             'pepper'],
-                "instructions": [
+            
+            "instructions": [
                 "Cook the noodles according to the package instructions until they are al dente. Drain and set aside.",
                 "Heat the vegetable oil in a large skillet or wok over medium-high heat.",
                 "Add the minced garlic and sliced onion to the skillet and stir-fry for about 1-2 minutes until fragrant and the onion becomes translucent.",
@@ -64,6 +69,7 @@ recipes = [
         
         {
             'name': 'Fried Rice',
+            
             'ingredients': ["2 cloves of garlic, minced", 
                             "1 small onion, diced", 
                             "1 carrot, diced", 
@@ -75,7 +81,8 @@ recipes = [
                             "2 green onions, sliced",
                             "Salt and pepper to taste", 
                             "Optional toppings: fried egg, cooked shrimp, diced chicken, chopped cilantro"],
-                "instructions": [
+            
+            "instructions": [
                 "Heat the vegetable oil in a large skillet or wok over medium-high heat."
                 "Add the minced garlic and diced onion to the skillet and stir-fry for about 1-2 minutes until fragrant and the onion becomes translucent."
                 "Add the diced carrot and cook for another 2-3 minutes until slightly softened."
@@ -88,6 +95,7 @@ recipes = [
                 "Remove from heat and transfer the fried rice to serving plates or bowls."
                 "Optional: Top with a fried egg, cooked shrimp, diced chicken, or chopped cilantro for added flavor and presentation."
                 "Serve the fried rice hot as a main dish or as a side dish with your choice of protein."],
+            
             'nutrients': ["Calories: 300-400", 
                           "Carbohydrates: 50-60 grams", 
                           "Protein: 6-8 grams", 
@@ -100,36 +108,132 @@ recipes = [
         
         {
             'name': 'Omelette',
+            
+            'ingredients': ["2 large eggs",
+                            "1 tablespoon milk or water",
+                            "Salt and pepper to taste",
+                            "1 tablespoon butter or cooking oil",
+                            "1/4 cup shredded cheese (such as cheddar, mozzarella, or Swiss)",
+                            "Optional fillings: diced vegetables (bell peppers, onions, mushrooms), cooked bacon, diced ham, herbs (parsley, chives), or any other desired ingredients."],
+            
+            "instructions": ["""In a bowl, whisk together the eggs, milk or water, salt, and pepper until well combined.
+Optional: If using any fillings, pre-cook and prepare them beforehand."
+Heat the butter or oil in a non-stick frying pan over medium heat. Ensure the pan is well
+coated with the fat.
+Once the butter has melted or the oil is heated, pour the beaten egg mixture into the
+pan, tilting it to spread the eggs evenly.
+Let the eggs cook undisturbed for a minute or until the edges begin to set.
+Using a spatula, gently lift the edges of the omelette and tilt the pan to allow any
+uncooked egg to flow to the edges.
+Sprinkle the shredded cheese and any desired fillings over one-half of the omelette.
+Fold the other half of the omelette over the filling to form a half-moon shape.
+Continue cooking the omelette for another minute or until the cheese has melted and the
+eggs are fully cooked but still moist.
+10. Slide the omelette onto a plate and serve immediately."""],
+            
+            'nutrients': ["Calories: 200-250" 
+                          "Carbohydrates: 1-2 grams" 
+                          "Protein: 14-16 grams" 
+                          "Fat: 15-18 grams Fiber: 0"
+                          "grams Sodium: 300-400 milligrams" 
+                          "Vitamin A: 15-20% (of the recommended daily intake Vitamin)"
+                          "C: 0% Calcium: 10-15% (of the recommended daily intake)" 
+                          "Iron: 6-8% (of the recommended daily intake)"]
+        },
+        
+        {
+            'name': 'Tacos',
+            
+            'ingredients': ["1 pound (450 grams) ground beef",
+                            "1 tablespoon cooking oil",
+                            "1 small onion, diced",
+                            "2 cloves of garlic, minced",
+                            "1 teaspoon chili powder",
+                            "1/2 teaspoon ground cumin",
+                            "1/2 teaspoon paprika",
+                            "Salt and pepper to taste",
+                            "8 small corn or flour tortillas",
+                            "Optional toppings: shredded lettuce, diced tomatoes, grated cheese, sliced avocado,sour cream, salsa, cilantro, lime wedges"],
+            
+            "instructions": ["""
+1. Heat the cooking oil in a large skillet over medium heat.
+2. Add the diced onion to the skillet and cook for 2-3 minutes until it becomes translucent.
+3. Add the minced garlic to the skillet and cook for another minute until fragrant.
+4. Increase the heat to medium-high and add the ground beef to the skillet. Break up the
+meat using a spatula or wooden spoon and cook until it is browned and cooked through.
+5. Drain any excess fat from the skillet, if desired.
+6. Add the chili powder, ground cumin, paprika, salt, and pepper to the skillet. Stir well to
+combine and cook for an additional 2-3 minutes to allow the flavors to meld.
+7. Warm the tortillas according to the package instructions or by heating them on a dry
+skillet for a few seconds on each side.
+8. Fill each tortilla with a portion of the beef mixture, and add desired toppings such as
+shredded lettuce, diced tomatoes, grated cheese, sliced avocado, sour cream, salsa,
+cilantro, or a squeeze of lime juice.
+9. Serve the tacos warm and enjoy!
+"""],
+            
+            'nutrients': ["Calories: 400-500", 
+                          "Carbohydrates: 30-35 grams", 
+                          "Protein: 20-25 grams", 
+                          "Fat: 20-25 grams", 
+                          "Fiber: 3-5 grams", 
+                          "Sodium: 400-500 milligrams", 
+                          "Vitamin A: 10-15% (of the recommended daily intake)", 
+                          "Vitamin C: 8-10% (of the recommended daily intake)", 
+                          "Calcium: 10-15% (of the recommended daily intake)",
+                          "Iron: 15-20% (of the recommended daily intake)"
+                          ]
+        },
+        
+        {
+            'name': 'Quesadillas',
+           
+            'ingredients': ["4 large flour tortillas",
+                            "2 cups shredded cheese (such as cheddar, Monterey Jack, or a blend)",
+                            "Optional fillings: diced cooked chicken, cooked ground beef, sliced peppers, onions, diced tomatoes, chopped cilantro, or any other desired ingredients.",
+                            "Cooking oil or cooking spray"],
+            
+            "instructions": ["""1. Preheat a large non-stick skillet or griddle over medium heat.
+2. If using any fillings, pre-cook and prepare them beforehand.
+3. Place one tortilla flat on a clean surface.
+4. Sprinkle about 1/2 cup of shredded cheese evenly over half of the tortilla.
+5. Add any desired fillings on top of the cheese.
+6. Fold the other half of the tortilla over the filling to create a half-moon shape.
+7. Repeat the process with the remaining tortillas and fillings.
+8. Lightly grease the preheated skillet or griddle with cooking oil or cooking spray.
+9. Place one quesadilla onto the skillet and cook for about 2-3 minutes, or until the bottom
+tortilla becomes golden brown and crispy.
+10. Flip the quesadilla using a spatula and cook for another 2-3 minutes on the other side
+until the cheese is melted and the tortilla is crispy.
+11. Remove the cooked quesadilla from the skillet and repeat the process with the remaining
+quesadillas.
+12. Allow the quesadillas to cool for a minute, then slice them into wedges or triangles.
+13. Serve the quesadillas warm with your choice of toppings, such as salsa, guacamole,
+sour cream, or pico de gallo."""],
+            
+            'nutrients': ["Calories: 300-400", 
+                          "Carbohydrates: 30-40 grams", 
+                          "Protein: 10-15 grams", 
+                          "Fat: 15-20 grams", 
+                          "Fiber:2-3 grams", 
+                          "Sodium: 500-600 milligrams", 
+                          "Vitamin A: 10-15% (of the recommended daily intake)",
+                          "Vitamin C: 2-4% (of the recommended daily intake)", 
+                          "Calcium: 20-25% (of the recommended daily intake)", 
+                          "Iron: 10-15% (of the recommended daily intake)"]
+        },
+        
+        {
+            'name': '',
             'ingredients': [],
-                "instructions": [],
+            "instructions": [],
             'nutrients': []
         },
         
         {
             'name': '',
             'ingredients': [],
-                "instructions": [],
-            'nutrients': []
-        },
-        
-        {
-            'name': '',
-            'ingredients': [],
-                "instructions": [],
-            'nutrients': []
-        },
-        
-        {
-            'name': '',
-            'ingredients': [],
-                "instructions": [],
-            'nutrients': []
-        },
-        
-        {
-            'name': '',
-            'ingredients': [],
-                "instructions": [],
+            "instructions": [],
             'nutrients': []
         }
 ]
